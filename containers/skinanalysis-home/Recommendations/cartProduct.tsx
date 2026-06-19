78ca14489c1db1ae58fe77b3179ff17142f91b98
Ranjith <cranjithkumar333@gmail.com>
2026-06-19 11:47:34 +0530

    coupon disabled

--- containers/skinanalysis-home/Recommendations/cartProduct.tsx
+++ containers/skinanalysis-home/Recommendations/cartProduct.tsx
@@ -671,7 +671,7 @@ const CartProduct: React.FC<CartProductProps> = ({ open, onClose, onCheckout }) 
                                     </Box>
                                 </Box>
 
-                                <Box sx={{ mt: 2, bgcolor: "#fff", borderRadius: 2, p: 2, border: "2px solid #316D52" }}>
+                                {/* <Box sx={{ mt: 2, bgcolor: "#fff", borderRadius: 2, p: 2, border: "2px solid #316D52" }}>
                                     <Box sx={{ display: "flex", alignItems: "center", gap: 1, mb: 1.5 }}>
                                         <Box sx={{ color: "#316D52", display: "flex", alignItems: "center" }}>
                                             <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor"><path d="M21.41 11.58l-9-9C12.05 2.22 11.55 2 11 2H4c-1.1 0-2 .9-2 2v7c0 .55.22 1.05.59 1.42l9 9c.36.36.86.58 1.41.58s1.05-.22 1.41-.59l7-7c.37-.36.59-.86.59-1.41s-.23-1.06-.59-1.42zM5.5 7C4.67 7 4 6.33 4 5.5S4.67 4 5.5 4 7 4.67 7 5.5 6.33 7 5.5 7z" /></svg>
@@ -733,7 +733,7 @@ const CartProduct: React.FC<CartProductProps> = ({ open, onClose, onCheckout }) 
                                             <CloseIcon sx={{ fontSize: 20 }} />
                                         </IconButton>
                                     </Box>
-                                )}
+                                )} */}
                             </>
                         ) : items.length === 0 ? (
                             <Box sx={{ py: 2, textAlign: "center" }}>
